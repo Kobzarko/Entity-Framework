713b6ecfa0cbc0985fb580a2e4d97a45010f1d65
Yevhen <evgewi4@gmail.com>
2020-02-12 15:01:10 +0200

    added files

---
+++ EntityCodeSecond/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCodeSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                var users = db.Users;
+                foreach (User item in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", item.Id, item.Name, item.Age);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
